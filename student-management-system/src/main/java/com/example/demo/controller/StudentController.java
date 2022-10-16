8291da10135da8ccdaf3cf1b0910aa907fb81a4d
Burak Güllüler <burakgulluler@gmail.com>
2022-10-16 13:32:37 +0300

    Final

--- student-management-system/src/main/java/com/example/demo/controller/StudentController.java
+++ student-management-system/src/main/java/com/example/demo/controller/StudentController.java
@@ -60,6 +60,13 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
+	//handler method to handle delete student request
+	@GetMapping("/students/{id}")
+	public String deleteStudent(@PathVariable Long id) {
+		studentService.deleteStudentById(id);
+		return "redirect:/students";
+	}
+	
 }
 
 
